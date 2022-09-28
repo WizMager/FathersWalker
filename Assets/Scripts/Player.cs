373d65ca7201ea670c7d1e501f5ae7efc123dba5
WizMager <wizmager@gmail.com>
2022-09-28 22:03:18 +0300

    feat: configure all, ready to start(just tested one DoTween method =) )

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    //private Transform _firstPosition;
+    private InputActions _inputActions;
+    private void Awake()
+    {
+        _inputActions = new InputActions();
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.TouchInput.Touch.Enable();
+        _inputActions.TouchInput.Touch.performed += OnTouch;
+    }
+
+    private void OnTouch(InputAction.CallbackContext obj)
+    {
+        //transform.DOMove(_firstPosition.position, 3f);
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.TouchInput.Touch.performed -= OnTouch;
+        _inputActions.TouchInput.Touch.Disable();
+    }
+}
---
+++ Assets/Scripts/Start.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Start : MonoBehaviour
+{
+      [SerializeField] private GameObject player;
+      [SerializeField] private Transform playerSpawnPosition;
+
+      private void Awake()
+      {
+            Instantiate(player, playerSpawnPosition.position, playerSpawnPosition.rotation);
+      }
+}
