73dd3bb2f53699347ecb2a117ee47da1e1f85eee
WizMager <wizmager@gmail.com>
2022-10-01 13:40:41 +0300

    feat: add more point and loop it feat/note: add comment how to use Checkpoint.cs

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -3,6 +3,10 @@
 public class Checkpoint : MonoBehaviour
 {
         [SerializeField] private MoveDirection[] moveDirections;
+        //If you use "deadEnd" character rotate to point from which it come
+        //If you use "stopCheckpoint" you need rotate how player will stay on this point(forward will be forward on this point)
+        //If you use point like waypoint(only for walk through) you need add next point and previous in "nextCheckpoint"
+        //"nextCheckpoint" : 0 element is turn left, 1 is turn right, 2 is turn forward
         [SerializeField] private Checkpoint[] nextCheckpoints;
         [SerializeField] private Transform pointTransform;
         [SerializeField] private bool deadEnd;
